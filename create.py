f4132acdbf4a9b86f33e64536a07fa7a23dc9e14
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-06-27 15:24:25 -0500

    black

--- create.py
+++ create.py
@@ -2,23 +2,28 @@ import pysnow
 import config
 from pprint import pprint
 
-c = pysnow.Client(instance=config.snow_instance, user=config.snow_username, password=config.snow_password)
+c = pysnow.Client(
+    instance=config.snow_instance,
+    user=config.snow_username,
+    password=config.snow_password,
+)
 
-incident = c.resource(api_path='/table/incident')
-short_description = 'PySNOW is awesome!'
-description = 'Incident created programmatically'
+incident = c.resource(api_path="/table/incident")
+short_description = "PySNOW is awesome!"
+description = "Incident created programmatically"
+
 
 def incident_exists(short_description):
-    result = incident.get(query={'short_description': short_description}, stream=True)
+    result = incident.get(query={"short_description": short_description}, stream=True)
     return bool(result.first_or_none())
 
+
 if not incident_exists(short_description):
-    new_record = {
-        'short_description': short_description,
-        'description': description
-    }
+    new_record = {"short_description": short_description, "description": description}
     result = incident.create(payload=new_record)
     if result:
-        print('Created new incident!')
+        print("Created new incident!")
 else:
-    print(f'Incident with short description "{short_description}" already exists, doing nothing')
+    print(
+        f'Incident with short description "{short_description}" already exists, doing nothing'
+    )
--- fetch.py
+++ fetch.py
@@ -2,13 +2,17 @@ import pysnow
 import config
 from pprint import pprint
 
-c = pysnow.Client(instance=config.snow_instance, user=config.snow_username, password=config.snow_password)
+c = pysnow.Client(
+    instance=config.snow_instance,
+    user=config.snow_username,
+    password=config.snow_password,
+)
 
-incident = c.resource(api_path='/table/incident')
+incident = c.resource(api_path="/table/incident")
 
-response = incident.get(query={'state': 1}, stream=True)
+response = incident.get(query={"number": "INC2275909"}, stream=True)
 
-#for record in response.all():
+# for record in response.all():
 #    pprint(record)
 
 pprint(response.first_or_none())
